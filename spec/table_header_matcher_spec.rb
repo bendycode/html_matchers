d8b2d6aa21db1be9472526cc186c3bad32e70efd
Stephen Anderson <stephen@bendycode.com>
2008-09-26 18:31:24 -0500

    Small change to test github to lighthouse post commit hooks. [#1 state:open]

--- spec/table_header_matcher_spec.rb
+++ spec/table_header_matcher_spec.rb
@@ -9,7 +9,7 @@ describe 'table_header_matcher' do
 	end
 
 	describe 'without <thead> element' do
-		it 'should not require thead' do
+		it 'should not require <thead>' do
 			response = mock_model(Object, :body => '<table id="my_id"><tr><th>h1</th><th>h2</th></tr></table>')
 			response.should have_table_header('my_id', [['h1', 'h2']])
 		end
