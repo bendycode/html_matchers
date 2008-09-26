196fb11906eeee422ce76c42b69eb8f6628860ec
Stephen Anderson <stephen@bendycode.com>
2008-09-26 18:38:18 -0500

    Small change to test github to lighthouse post commit hooks [#1 status:open]

--- spec/table_header_matcher_spec.rb
+++ spec/table_header_matcher_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe 'table_header_matcher' do
-	describe 'with interposing <thead> element' do
+	describe 'with <thead> element' do
 		it 'should find headers' do
 			response = mock_model(Object, :body => '<table id="my_id"><thead><tr><th>h1</th><th>h2</th></tr></thead></table>')
 			response.should have_table_header('my_id', [['h1', 'h2']])
