2a6fd630a594d7245687e3fc937b8b0de1c073e2
Stephen Anderson <stephen@bendycode.com>
2008-09-26 21:32:35 -0500

    Renamed the helper method. [#1 state:resolved]

--- spec/table_header_matcher_spec.rb
+++ spec/table_header_matcher_spec.rb
@@ -3,27 +3,27 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe 'table_header_matcher' do
 	describe 'with <thead> element' do
 		it 'should find headers' do
-			verify_table_header_matcher 'my_id', '<table id="my_id"><thead><tr><th>h1</th><th>h2</th></tr></thead></table>', [['h1', 'h2']]
+			verify_table_header_match 'my_id', '<table id="my_id"><thead><tr><th>h1</th><th>h2</th></tr></thead></table>', [['h1', 'h2']]
 		end
 	end
 
 	describe 'without <thead> element' do
 		it 'should not require <thead>' do
-			verify_table_header_matcher 'my_id', '<table id="my_id"><tr><th>h1</th><th>h2</th></tr></table>', [['h1', 'h2']]
+			verify_table_header_match 'my_id', '<table id="my_id"><tr><th>h1</th><th>h2</th></tr></table>', [['h1', 'h2']]
 		end
 
 		it 'should ignore regular row' do
-			verify_table_header_matcher 'my_id', '<table id="my_id"><tr><th>h1</th><th>h2</th></tr><tr><td>"regular"</td><td>"row"</td></tr></table>', [['h1', 'h2']]
+			verify_table_header_match 'my_id', '<table id="my_id"><tr><th>h1</th><th>h2</th></tr><tr><td>"regular"</td><td>"row"</td></tr></table>', [['h1', 'h2']]
 		end
 
 		it 'should handle multiple header rows' do
-			verify_table_header_matcher 'my_id', '<table id="my_id"><tr><th>h1</th><th>h2</th></tr><tr><th>h3</th><th>h4</th></tr><tr><td>"regular"</td><td>"row"</td></tr></table>', [['h1', 'h2'], ['h3', 'h4']]
+			verify_table_header_match 'my_id', '<table id="my_id"><tr><th>h1</th><th>h2</th></tr><tr><th>h3</th><th>h4</th></tr><tr><td>"regular"</td><td>"row"</td></tr></table>', [['h1', 'h2'], ['h3', 'h4']]
 		end
 	end
 
 	private
 
-	def verify_table_header_matcher id, html, expected
+	def verify_table_header_match id, html, expected
 		response = mock_model Object, :body => html
 		response.should have_table_header(id, expected)
 	end
