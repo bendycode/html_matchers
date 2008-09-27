48f6a28aea6f65471af0e922f9c96d090f9a551b
Stephen Anderson <stephen@bendycode.com>
2008-09-26 21:28:05 -0500

    Extracted verify_table_header_matcher helper method. [#1 state:resolved] [#6 state:open]

--- spec/table_header_matcher_spec.rb
+++ spec/table_header_matcher_spec.rb
@@ -3,25 +3,28 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe 'table_header_matcher' do
 	describe 'with <thead> element' do
 		it 'should find headers' do
-			response = mock_model(Object, :body => '<table id="my_id"><thead><tr><th>h1</th><th>h2</th></tr></thead></table>')
-			response.should have_table_header('my_id', [['h1', 'h2']])
+			verify_table_header_matcher 'my_id', '<table id="my_id"><thead><tr><th>h1</th><th>h2</th></tr></thead></table>', [['h1', 'h2']]
 		end
 	end
 
 	describe 'without <thead> element' do
 		it 'should not require <thead>' do
-			response = mock_model(Object, :body => '<table id="my_id"><tr><th>h1</th><th>h2</th></tr></table>')
-			response.should have_table_header('my_id', [['h1', 'h2']])
+			verify_table_header_matcher 'my_id', '<table id="my_id"><tr><th>h1</th><th>h2</th></tr></table>', [['h1', 'h2']]
 		end
 
 		it 'should ignore regular row' do
-			response = mock_model(Object, :body => '<table id="my_id"><tr><th>h1</th><th>h2</th></tr><tr><td>"regular"</td><td>"row"</td></tr></table>')
-			response.should have_table_header('my_id', [['h1', 'h2']])
+			verify_table_header_matcher 'my_id', '<table id="my_id"><tr><th>h1</th><th>h2</th></tr><tr><td>"regular"</td><td>"row"</td></tr></table>', [['h1', 'h2']]
 		end
 
 		it 'should handle multiple header rows' do
-			response = mock_model(Object, :body => '<table id="my_id"><tr><th>h1</th><th>h2</th></tr><tr><th>h3</th><th>h4</th></tr><tr><td>"regular"</td><td>"row"</td></tr></table>')
-			response.should have_table_header('my_id', [['h1', 'h2'], ['h3', 'h4']])
+			verify_table_header_matcher 'my_id', '<table id="my_id"><tr><th>h1</th><th>h2</th></tr><tr><th>h3</th><th>h4</th></tr><tr><td>"regular"</td><td>"row"</td></tr></table>', [['h1', 'h2'], ['h3', 'h4']]
 		end
+	end
+
+	private
+
+	def verify_table_header_matcher id, html, expected
+		response = mock_model Object, :body => html
+		response.should have_table_header(id, expected)
 	end
 end
