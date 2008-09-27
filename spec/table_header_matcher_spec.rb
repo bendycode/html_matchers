5b408a0eafe15b13592500d38f43bf4e696c07f6
Stephen Anderson <stephen@bendycode.com>
2008-09-26 21:16:11 -0500

    Added a test that shows we handle multiple header rows [#6 status:open]

--- spec/table_header_matcher_spec.rb
+++ spec/table_header_matcher_spec.rb
@@ -18,5 +18,10 @@ describe 'table_header_matcher' do
 			response = mock_model(Object, :body => '<table id="my_id"><tr><th>h1</th><th>h2</th></tr><tr><td>"regular"</td><td>"row"</td></tr></table>')
 			response.should have_table_header('my_id', [['h1', 'h2']])
 		end
+
+		it 'should handle multiple header rows' do
+			response = mock_model(Object, :body => '<table id="my_id"><tr><th>h1</th><th>h2</th></tr><tr><th>h3</th><th>h4</th></tr><tr><td>"regular"</td><td>"row"</td></tr></table>')
+			response.should have_table_header('my_id', [['h1', 'h2'],['h3', 'h4']])
+		end
 	end
 end
