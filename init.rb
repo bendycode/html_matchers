a12b137d5edb1c4e93c106545d06cd325386b205
Stephen Anderson <stephen@bendycode.com>
2008-09-27 16:49:44 -0500

    Removed bogus, commented out code.

--- init.rb
+++ init.rb
@@ -1,11 +1,2 @@
 # Include hook code here
 require File.join(File.dirname(__FILE__), 'lib', 'html_matchers')
-
-# module Spec # :nodoc:
-#   module Rails
-#     module Matchers
-# 			include HtmlMatchers
-# 		end
-# 	end
-# end
-# Spec::Rails::Matchers.send :include, HtmlMatchers
