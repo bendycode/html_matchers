0a20df3237ee040c675fcd01485e8846eeb27090
Hani Elabed <hani.elabed@gmail.com>
2009-05-25 20:15:20 -0500

    refined table_header_matcher to better deal with white space. suggested refinements(commented out code) for the table_matcher.

--- lib/matchers/table_matcher.rb
+++ lib/matchers/table_matcher.rb
@@ -32,9 +32,11 @@ module Spec # :nodoc:
 
 					rows = doc.search("table#{"##{@table_id}" if @table_id} tr")
 					header_elements = rows.reject{|e| e.search('th').empty? }
+					#header_content = header_elements.map{|n| n.search('/th').map{|n| n.inner_text.strip.gsub(/\n[\n \t]*/, "\n")}}
 					header_content = header_elements.map{|n| n.search('/th').map{|n| n.inner_text.strip.gsub(/\n    \t\t/, "\n")}}
 
 					body_elements = rows.reject{|e| e.search('td').empty? }
+					#body_content = body_elements.map{|n| n.search('/td').map{|n| n.inner_text.strip.gsub(/\n[\n \t]*/, "\n")}}
 					body_content = body_elements.map{|n| n.search('/td').map{|n| n.inner_text.strip.gsub(/\n    \t\t/, "\n")}}
 
           header_content + body_content
